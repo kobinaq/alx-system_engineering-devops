49e613f60d43c0f4f45c470e6b216a13e7273924
kobinaq <kobinastudios@gmail.com>
2024-06-29 06:16:46 +0000

    Task 1 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 #installing flask
 package { 'flask':
-
   ensure   => '2.1.0',
   provider => 'pip3',
+  command  => '/usr/bin/pip3',
+}
+
+-> exec { 'verify flask installation':
+  command     => '/usr/bin/flask --version',
+  refreshonly => true,
+  subscribe   => Package['flask'],
+  path        => ['/usr/local/bin', '/usr/bin', '/bin'],
 }
